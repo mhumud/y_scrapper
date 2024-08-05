f7d099bdaaf536e24d12e385caec0b8e937e4f49
Miguel <miguelhumudr@gmail.com>
2024-08-05 15:53:31 +0100

    Add both cases for scraper test

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -12,6 +12,13 @@ def test_scrape_news(monkeypatch):
 
     entries = scrape_news()
 
-    assert len(entries) == 2  # Assert the number of scraped entries
+    assert len(entries) == 2  #
+    assert entries[0]['number'] == 1
     assert entries[0]['title'] == "Show HN: Free e-book about WebGPU Programming"
+    assert entries[0]['points'] == 187
+    assert entries[0]['comments'] == 19
+
+    assert entries[1]['number'] == 2
     assert entries[1]['title'] == "How I Program in 2024"
+    assert entries[1]['points'] == 228
+    assert entries[1]['comments'] == 150
